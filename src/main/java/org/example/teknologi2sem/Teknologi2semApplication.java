4d236ce94827ca8abdc35363267ca5497d3d5988
jshar <joha0013@stud.ek.dk>
2026-03-24 10:15:30 +0100

    ad

---
+++ src/main/java/org/example/teknologi2sem/Teknologi2semApplication.java
@@ -0,0 +1,13 @@
+package org.example.teknologi2sem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teknologi2semApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Teknologi2semApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/teknologi2sem/Teknologi2semApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.teknologi2sem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Teknologi2semApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
